57b5ecbb01e22af02a474f49a41a4bc1cefcea32
hharden11 <hharden11@gmailcom>
2015-02-25 19:53:56 -0600

    Finished Part I

--- Assignment6/artistNetworks.py
+++ Assignment6/artistNetworks.py
@@ -1,10 +1,11 @@
 import requests
+import pandas as pd
 
 def getRelatedArtists(artistID):
     """Using the Spotify Get an Artist's Related Artists API, take an individual artist's ID 
     and return the IDs of up to 20 related artists."""
     url = "https://api.spotify.com/v1/artists/" + artistID +"/related-artists"
-    print url
+    #print url
     req = requests.get(url)
     raw_data = req.json()
     related_artists_list = []
@@ -14,15 +15,32 @@ def getRelatedArtists(artistID):
 print getRelatedArtists("2mAFHYBasVVtMekMUkRO9g")
 
 def getDepthEdges(artistID, depth):
-'''takes two arguments, an artist ID and an integer depth, 
-and returns a list of tuples representing the (directed) pairs of related artists'''
+    '''takes two arguments, an artist ID and an integer depth, and returns a 
+    list of tuples representing the (directed) pairs of related artists'''
+    original_list = getRelatedArtists(artistID)
+    tuple_list = []
+    for i in original_list:
+        tuple_list.append((artistID,i))
+    if depth == 1:
+        return tuple_list
+    else:
+        for i in original_list:
+            tuple_list.extend(getDepthEdges(i,depth-1))
+        return tuple_list
+print getDepthEdges("2mAFHYBasVVtMekMUkRO9g",2)
 
+def getEdgeList(artistID, depth):
+    '''takes the exact same arguments as getDepthEdges(), but returns the result 
+    as a Pandas DataFrame with one row for each edge'''
+    data = getDepthEdges(artistID,depth)
+    df = pd.DataFrame(data)
+    return df
+print getEdgeList("2mAFHYBasVVtMekMUkRO9g",2)
 
-#def getEdgeList(artistID, depth):
-''' takes the exact same arguments as getDepthEdges(),
- but returns the result as a Pandas DataFrame with one row for each edge'''
-
-#def writeEdgeList(artistID, depth, filename):
-'''takes three arguments: an artist ID, a depth value, and a filename for output. 
-This function generates an edge list based on the parameters artistId and depth, 
-and writes that to a CSV file specified by the filename parameter'''
+def writeEdgeList(artistID, depth, filename):
+    '''takes three arguments: an artist ID, a depth value, and a filename for output. 
+    This function generates an edge list based on the parameters artistId and depth, 
+    and writes that to a CSV file specified by the filename parameter'''
+    df = getEdgeList(artistID, depth)
+    df.to_csv(filename,index=False,quotechar='"')
+print writeEdgeList("2mAFHYBasVVtMekMUkRO9g",2,"edges.csv")
